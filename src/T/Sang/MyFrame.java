333b9b9600cd148ec0cf779b2adfba60029483d5
Hiam-Kaito <52872325+Hiam-Kaito@users.noreply.github.com>
2020-04-09 15:45:38 +0700

    Java Swing

--- src/T/Sang/MyFrame.java
+++ src/T/Sang/MyFrame.java
@@ -16,8 +16,8 @@ public class MyFrame{
     Panel p2 = new Panel();
     Label lbl1 = new Label("Mã Sinh Viên");
     Label lbl2 = new Label("Họ và tên");
-    TextField tf1 = new TextField();
-    TextField tf2 = new TextField();
+    JTextField tf1 = new JTextField();
+    JTextField tf2 = new JTextField();
     Font Arial = new Font("Arial", Font.BOLD, 40);
     
     private void initComponents() {
@@ -37,10 +37,10 @@ public class MyFrame{
         lbl2.setBounds(50, 100, 250, 50);
         lbl2.setFont(Arial);
         
-        tf1.setBounds(100, 50, 550, 50);
+        tf1.setBounds(400, 50, 550, 50);
         tf1.setFont(Arial);
         
-        tf2.setBounds(100, 100, 550, 50);
+        tf2.setBounds(400, 100, 550, 50);
         tf2.setFont(Arial);
         
         frame.add(p1);
