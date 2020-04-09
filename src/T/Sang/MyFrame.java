24d59f1300fa17e8b6c2dc7d5cfef8df16aa44e5
Hiam-Kaito <52872325+Hiam-Kaito@users.noreply.github.com>
2020-04-09 15:24:06 +0700

    Java Swing

--- src/T/Sang/MyFrame.java
+++ src/T/Sang/MyFrame.java
@@ -11,29 +11,48 @@ public class MyFrame{
         initComponents();
     }
     
-    JFrame frame = new JFrame();
+    static JFrame frame = new JFrame();
     Panel p1 = new Panel();
     Panel p2 = new Panel();
-//    Panel p3 = new Panel();
+    Label lbl1 = new Label("Mã Sinh Viên");
+    Label lbl2 = new Label("Họ và tên");
+    TextField tf1 = new TextField();
+    TextField tf2 = new TextField();
+    Font Arial = new Font("Arial", Font.BOLD, 40);
     
     private void initComponents() {
-        p1.setBounds(0, 0, 50, 50);
-        p2.setBounds(50, 50, 50, 50);
-
-        p1.setBackground(Color.yellow);
+        p1.setBounds(0, 0, 2000, 200);
+        p1.setLayout(null);
+        p1.add(lbl1);
+        p1.add(lbl2);
+        p1.add(tf1);
+        p1.add(tf2);
+        p1.setBackground(Color.cyan);
+        
         p2.setBackground(Color.blue);
+        
+        lbl1.setBounds(50, 50, 250, 50);
+        lbl1.setFont(Arial);
+        
+        lbl2.setBounds(50, 100, 250, 50);
+        lbl2.setFont(Arial);
+        
+        tf1.setBounds(100, 50, 550, 50);
+        tf1.setFont(Arial);
+        
+        tf2.setBounds(100, 100, 550, 50);
+        tf2.setFont(Arial);
         
         frame.add(p1);
         frame.add(p2);
         
         frame.setLayout(null);
-        frame.setSize(500, 500);
-        
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
     
     public static void main(String[] args) {
         new MyFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
